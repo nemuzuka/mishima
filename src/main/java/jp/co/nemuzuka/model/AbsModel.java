66ca988b9e513cad0416d9e2b5460cfd201dd9cb
Kazumune Katagiri <kkaz123@gmail.com>
2012-04-16 22:39:06 +0900

    Modelの共通項目見直し 必要なmodelにのみ設定する方針とする

--- src/main/java/jp/co/nemuzuka/model/AbsModel.java
+++ src/main/java/jp/co/nemuzuka/model/AbsModel.java
@@ -1,18 +1,9 @@
 package jp.co.nemuzuka.model;
 
-import java.util.Date;
-
-import net.arnx.jsonic.JSONHint;
-
 import org.slim3.datastore.Attribute;
-import org.slim3.datastore.CreationDate;
-import org.slim3.datastore.CreationUser;
 import org.slim3.datastore.Datastore;
-import org.slim3.datastore.ModificationDate;
-import org.slim3.datastore.ModificationUser;
 
 import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.users.User;
 
 /**
  * Modelの基底クラス.
@@ -21,22 +12,6 @@ import com.google.appengine.api.users.User;
 public abstract class AbsModel {
 
 	//共通的に使用する
-	/** 登録日時. */
-	@Attribute(listener = CreationDate.class, unindexed=true)
-	private Date createdAt;
-	
-	/** 登録ユーザ. */
-	@Attribute(listener = CreationUser.class, unindexed=true)
-	private User createUser;
-	
-	/** 更新日時. */
-	@Attribute(listener = ModificationDate.class, unindexed=true)
-	private Date updatedAt;
-	
-	/** 更新ユーザ. */
-	@Attribute(listener = ModificationUser.class, unindexed=true)
-	private User updateUser;
-
 	/** バージョンNo. */
 	@Attribute(version = true)
 	private Long version;
@@ -59,66 +34,6 @@ public abstract class AbsModel {
 	 */
 	public String getKeyToString() {
 		return Datastore.keyToString(getKey());
-	}
-
-	/**
-	 * @return createdAt
-	 */
-	@JSONHint(ignore=true)
-	public Date getCreatedAt() {
-		return createdAt;
-	}
-
-	/**
-	 * @param createdAt セットする createdAt
-	 */
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
-
-	/**
-	 * @return createUser
-	 */
-	@JSONHint(ignore=true)
-	public User getCreateUser() {
-		return createUser;
-	}
-
-	/**
-	 * @param createUser セットする createUser
-	 */
-	public void setCreateUser(User createUser) {
-		this.createUser = createUser;
-	}
-
-	/**
-	 * @return updatedAt
-	 */
-	@JSONHint(ignore=true)
-	public Date getUpdatedAt() {
-		return updatedAt;
-	}
-
-	/**
-	 * @param updatedAt セットする updatedAt
-	 */
-	public void setUpdatedAt(Date updatedAt) {
-		this.updatedAt = updatedAt;
-	}
-
-	/**
-	 * @return updateUser
-	 */
-	@JSONHint(ignore=true)
-	public User getUpdateUser() {
-		return updateUser;
-	}
-
-	/**
-	 * @param updateUser セットする updateUser
-	 */
-	public void setUpdateUser(User updateUser) {
-		this.updateUser = updateUser;
 	}
 
 	/**
